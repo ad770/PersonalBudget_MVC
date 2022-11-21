1aabb9deebe675663a3dd45907524e6aa55c8496
ad770 <zuchowski.adrian@gmail.com>
2022-11-21 22:27:15 +0100

    Try to send time data to Controller

--- App/Controllers/Items.php
+++ App/Controllers/Items.php
@@ -5,42 +5,16 @@ namespace App\Controllers;
 use \Core\View;
 use \App\Models\Income;
 use \App\Models\Expense;
-use App\Models\Balance;
-use \App\Auth;
-use App\Models\RememberedLogin;
+use \App\Models\Balance;
 use \App\Flash;
 
 
-/**
- * Items controller (example)
- *
- * PHP version 7.0
- */
-//class Items extends \Core\Controller
 class Items extends Authenticated
 {
-    // /**
-    //  * Before filter - called before each action method
-    //  *
-    //  * @return void
-    //  */
-    // protected function before()
-    // {
-    //     parent::before();
-
-    //     $this->user = Auth::getUser();
-    // }
-
-    /**
-     * Incomes items
-     *
-     * @return void
-     */
     public function incomesAction()
     {
-        $categories = Income::getIncomeCategories();
         View::renderTemplate('Items/incomes.html', [
-            'incomeCategories' => $categories
+            'incomeCategories' => Income::getIncomeCategories()
         ]);
     }
 
@@ -51,24 +25,19 @@ class Items extends Authenticated
             Flash::addMessage('Przychód poprawnie dodano!');
             $this->redirect('/Items/incomes');
         } else {
+            Flash::addMessage('Coś poszło nie tak, spróbuj ponownie', Flash::WARNING);
             View::renderTemplate('Items/incomes.html', [
+                'incomeCategories' => Income::getIncomeCategories(),
                 'income' => $income
             ]);
         }
     }
 
-    /**
-     * Expenses items
-     *
-     * @return void
-     */
     public function expensesAction()
     {
-        $categories = Expense::getExpenseCategories();
-        $payments = Expense::getPaymentCategories();
         View::renderTemplate('Items/expenses.html', [
-            'expenseCategories' => $categories,
-            'paymentCategories' => $payments
+            'expenseCategories' => Expense::getExpenseCategories(),
+            'paymentCategories' => Expense::getPaymentCategories()
         ]);
     }
 
@@ -79,84 +48,38 @@ class Items extends Authenticated
             Flash::addMessage('Wydatek poprawnie dodano!');
             $this->redirect('/Items/expenses');
         } else {
+            Flash::addMessage('Coś poszło nie tak, spróbuj ponownie', Flash::WARNING);
             View::renderTemplate('Items/expenses.html', [
+                'expenseCategories' => Expense::getExpenseCategories(),
+                'paymentCategories' => Expense::getPaymentCategories(),
                 'expense' => $expense
             ]);
         }
     }
 
-    /**
-     * Balance items
-     *
-     * @return void
-     */
     public function balanceAction()
     {
-        $expensesData = Balance::getExpenses();
-        $incomesData = Balance::getIncomes();
-        $expensesChartData = Balance::getSumOfExpense();
-        $incomesChartData = Balance::getSumOfIncome();
-
-        View::renderTemplate('Items/balance.html', [
-            'expensesData' => $expensesData,
-            'incomesData' => $incomesData,
-            'expensesChartData' => $expensesChartData,
-            'incomesChartData' => $incomesChartData
-        ]);
+        $balanceUndenify = new Balance($_POST);
+        if ($balanceUndenify->prepare()) {
+            var_dump($balanceUndenify);
+            View::renderTemplate('Items/balance.html', [
+                'expensesData' => Balance::getExpenses(),
+                'incomesData' => Balance::getIncomes(),
+                'expensesChartData' => Balance::getTotalExpense(),
+                'incomesChartData' => Balance::getTotalIncome()
+            ]);
+        }
     }
 
-    /**
-     * Add a new item
-     *
-     * @return void
-     */
-    public function newAction()
+    public function setDateAction()
     {
-        echo "new action";
+        $balanceUndenify = new Balance($_POST);
+        var_dump($balanceUndenify);
+        if ($balanceUndenify->prepare()) {
+            $this->balanceAction();
+        } else {
+            Flash::addMessage('Coś poszło nie tak, spróbuj ponownie', Flash::WARNING);
+            $this->balanceAction();
+        }
     }
-
-    /**
-     * Show an item
-     *
-     * @return void
-     */
-    public function showAction()
-    {
-        echo "show action";
-    }
-
-    // public JsonResult AjaxMethod()
-    // {
-    //     string query = "SELECT ShipCity, COUNT(orderid) TotalOrders";
-    //     query += " FROM Orders WHERE ShipCountry = 'USA' GROUP BY ShipCity";
-    //     string constr = ConfigurationManager.ConnectionStrings["Constring"].ConnectionString;
-    //     List<object> chartData = new List<object>();
-    //     chartData.Add(new object[]
-    //                     {
-    //                         "ShipCity", "TotalOrders"
-    //                     });
-    //     using (SqlConnection con = new SqlConnection(constr))
-    //     {
-    //         using (SqlCommand cmd = new SqlCommand(query))
-    //         {
-    //             cmd.CommandType = CommandType.Text;
-    //             cmd.Connection = con;
-    //             con.Open();
-    //             using (SqlDataReader sdr = cmd.ExecuteReader())
-    //             {
-    //                 while (sdr.Read())
-    //                 {
-    //                     chartData.Add(new object[]
-    //                     {
-    //                         sdr["ShipCity"], sdr["TotalOrders"]
-    //                     });
-    //                 }
-    //             }
-
-    //             con.Close();
-    //         }
-    //     }
-
-    //     return Json(chartData);
-    // }
 }
--- App/Models/Balance.php
+++ App/Models/Balance.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use PDO;
-use \App\Models\User;
 use \App\Auth;
 use \Core\View;
 
@@ -35,6 +34,40 @@ class Balance extends \Core\Model
         };
     }
 
+    public function prepare()
+    {
+        if (!isset($this->balanceDateOption)) {
+            $this->balanceDateOption = 'current_month';
+        }
+        switch ($this->balanceDateOption) {
+            default:
+            case 'current_month':
+                $this->beginDate = date('Y-m-d', strtotime('first day of this month'));
+                $this->endDate = date('Y-m-d', strtotime("last day of this month"));
+                break;
+
+            case 'previous_month':
+                $this->beginDate = date('Y-m-d', strtotime("first day of last month"));
+                $this->endDate = date('Y-m-d', strtotime("last day of last month"));
+                break;
+
+            case 'current_year':
+                $this->beginDate = date('Y-m-d', strtotime("first day of january"));
+                $this->endDate = date('Y-m-d', strtotime("last day of december"));
+                break;
+
+            case 'undenify':
+                $this->beginDate = $_POST['postBeginDate'];
+                $this->endDate =  $_POST['postEndDate'];
+
+                if (!$this->endDate || !$this->beginDate) {
+                    return false;
+                }
+                break;
+        }
+        return true;
+    }
+
     public static function getIncomes()
     {
         $sql = 'SELECT incomes_category_assigned_to_users.name, incomes.income_category_assigned_to_user_id, incomes.amount, incomes.date_of_income, incomes.income_comment 
@@ -55,7 +88,7 @@ class Balance extends \Core\Model
 
     public static function getExpenses()
     {
-        $sql = 'SELECT expenses_category_assigned_to_users.name, payment_methods_assigned_to_users.name, expenses.expense_category_assigned_to_user_id, expenses.payment_method_assigned_to_user_id, expenses.amount, expenses.date_of_expense, expenses.expense_comment 
+        $sql = 'SELECT expenses_category_assigned_to_users.name as expense_cat, payment_methods_assigned_to_users.name as payment_method, expenses.expense_category_assigned_to_user_id, expenses.payment_method_assigned_to_user_id, expenses.amount, expenses.date_of_expense, expenses.expense_comment 
                 FROM expenses, expenses_category_assigned_to_users, payment_methods_assigned_to_users
                 WHERE expenses.user_id = :user_id 
                 AND expenses.expense_category_assigned_to_user_id = expenses_category_assigned_to_users.id 
@@ -131,30 +164,40 @@ class Balance extends \Core\Model
     }
 
 
-    public static function getSumOfIncome()
+    public static function getTotalIncome()
     {
-        $sql = 'SELECT incomes_category_assigned_to_users.name, SUM(incomes.amount) FROM incomes_category_assigned_to_users, incomes WHERE incomes.user_id = :user_id 
+        // $beginDate = date('Y-m-d', strtotime('first day of this month'));
+        // $endDate = date('Y-m-d', strtotime('last day of this month'));
+        $sql = 'SELECT incomes_category_assigned_to_users.name, SUM(incomes.amount) as incomeSum FROM incomes_category_assigned_to_users, incomes WHERE incomes.user_id = :user_id 
         AND incomes.income_category_assigned_to_user_id = incomes_category_assigned_to_users.id
+        AND incomes.date_of_income BETWEEN :beginDate AND :endDate
         GROUP BY incomes_category_assigned_to_users.name';
         $user_id = Auth::getUser();
         $db = static::getDB();
         $stmt = $db->prepare($sql);
         $stmt->bindValue(':user_id', $user_id->id, PDO::PARAM_INT);
+        $stmt->bindValue(':beginDate', $this->beginDate, PDO::PARAM_STR);
+        $stmt->bindValue(':endDate', $this->endDate, PDO::PARAM_STR);
 
         $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
 
         return $stmt->execute();
     }
 
-    public static function getSumOfExpense()
+    public static function getTotalExpense()
     {
-        $sql = 'SELECT expenses_category_assigned_to_users.name, SUM(expenses.amount) FROM expenses_category_assigned_to_users, expenses WHERE expenses.user_id = :user_id 
+        // $beginDate = date('Y-m-d', strtotime('first day of this month'));
+        // $endDate = date('Y-m-d', strtotime('last day of this month'));
+        $sql = 'SELECT expenses_category_assigned_to_users.name, SUM(expenses.amount) as expenseSum FROM expenses_category_assigned_to_users, expenses WHERE expenses.user_id = :user_id 
         AND expenses.expense_category_assigned_to_user_id = expenses_category_assigned_to_users.id
+        AND expenses.date_of_expense BETWEEN :beginDate AND :endDate
         GROUP BY expenses_category_assigned_to_users.name';
         $user_id = Auth::getUser();
         $db = static::getDB();
         $stmt = $db->prepare($sql);
         $stmt->bindValue(':user_id', $user_id->id, PDO::PARAM_INT);
+        $stmt->bindValue(':beginDate', $this->beginDate, PDO::PARAM_STR);
+        $stmt->bindValue(':endDate', $this->endDate, PDO::PARAM_STR);
 
         $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
 
