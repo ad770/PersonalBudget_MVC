abe79d715ef28cddd99c91a8eb411962b48f7f1c
ad770 <zuchowski.adrian@gmail.com>
2023-05-19 23:12:53 +0200

    Add limit section for expenses

--- public/js/limit.js
+++ public/js/limit.js
@@ -1,26 +1,47 @@
-document.getElementById('expense_cat').addEventListener('change', isLimit);
+const getDate = document.getElementById('expense_date');
+const getExpense = document.getElementById('expense_value');
 
-const date = document.getElementById('expense_date').value;
+let id = 0;
+let limitValue = 0;
+let spent = 0;
+let expenseValue = 0;
+let balance = 0;
+let limitLeft = 0;
 
-let getExpenseValue = 0;
 
 const hideLimitSection = () => {
     $('.limitSection').hide();
 };
 
 function isLimit() {
-    const id = this.options[this.selectedIndex].id;
-    this.value == 1 ? checkCategory(id) : hideLimitSection();
+    id = this.options[this.selectedIndex].id;
+    this.value == 1 ? showLimit() : hideLimitSection();
 };
 
-const checkCategory = (id) => {
+const showLimit = async () => {
     $('.limitSection').show(500);
-    // getLimitForCategory(id);
-    renderOnDom(id, date);
-    // checkLimit(id);
+    await calculateLimit();
+    await renderOnDom();
+}
+
+const changeValues = async () => {
+    await calculateLimit();
+    await renderOnDom();
+}
+
+const calculateLimit = async () => {
+    date = await getDate.value;
+    limitValue = await getLimitForCategory();
+    spent = await getSumOfExpensesForSelectedMonth();
+    expenseValue = getExpense.value;
+    if (expenseValue === null) {
+        expenseValue = 0;
+    }
+    balance = limitValue - spent;
+    limitLeft = balance - expenseValue;
 };
 
-const getLimitForCategory = async (id) => {
+const getLimitForCategory = async () => {
     try {
         const res = await fetch(`/api/limit/${id}`);
         const data = await res.json();
@@ -30,7 +51,7 @@ const getLimitForCategory = async (id) => {
     }
 };
 
-const getSumOfExpensesForSelectedMonth = async (id, date) => {
+const getSumOfExpensesForSelectedMonth = async () => {
     try {
         const res = await fetch(`/api/expenses/${id}/${date}`);
         const data = await res.json();
@@ -40,97 +61,18 @@ const getSumOfExpensesForSelectedMonth = async (id, date) => {
     }
 };
 
-
-// const checkLimit = (id) => {
-//     console.log(date);
-//     //getSumOfExpensesForSelectedMonth(id, date);
-
-
-//     // const date = new Date(document.getElementById('expense_date').value);
-//     // const month = date.toLocaleString('en-us', { month: 'long' });;
-//     // console.log(date);
-//     // console.log(month);
-//     // getSumOfExpensesForSelectedMonth(id, month);
-
-//     //calculateLimits();
-//     renderOnDom(id, date);
-// }
-
-function getValue() {
-    getExpenseValue = document.getElementById('expense_value').value;
-    console.log(getExpenseValue);
-};
-
-function getDate() {
-    getDateValue = document.getElementById('expense_date').value;
-    console.log(getDateValue);
-};
-
-// REST API
-const renderOnDom = async (id, date) => {
-    //getValue();
-    //getDate();
-    let limit = await getLimitForCategory(id);
-    let spent = await getSumOfExpensesForSelectedMonth(id, date);
-    let balance = limit - spent;
-    let limitLeft = balance - getExpenseValue;
-    document.getElementById('limit').innerHTML = limit + " zł";
-    document.getElementById('spent').innerHTML = spent + " zł";
+const renderOnDom = () => {
+    document.getElementById('limit').innerHTML = limitValue + " zł";
+    if (spent) {
+        document.getElementById('spent').innerHTML = spent + " zł";
+    } else {
+        document.getElementById('spent').innerHTML = "0 zł";
+    }
     document.getElementById('balance').innerHTML = balance + " zł";
     document.getElementById('spentAndValueInput').innerHTML = limitLeft + " zł";
-};
+}
 
-const calculateLimits = () => {
-    // getValue();
-
-    // let expenseLimit = parseFloat(document.getElementById('limit').innerHTML);
-    // let expenseSum = parseFloat(document.getElementById('spent').innerHTML);
-    // let newExpense = parseFloat(document.getElementById('expense_value').value);
-
-    // let sum = expenseSum + newExpense;
-    // let diff = expenseLimit - (expenseSum + newExpense);
-
-    // document.getElementById('balance').innerHTML = sum.toFixed(2);
-    // document.getElementById('spentAndValueInput').innerHTML = diff.toFixed(2);
-
-    // if (expenseLimit != 0){
-    //     if (diff > 0) {
-    //         $('.expenseLimit').css('color', 'green');
-    //     }
-
-    //     else if (diff < 0){
-    //         $('.expenseLimit').css('color', 'red');
-    //     }
-    // }
-};
-
-// const getSumOfExpensesForSelectedMonth = (id, date) => {
-//     fetch(`/api/expenses/${id}/${date}`)
-//         .then(response => response.json())
-//         .then(data => {
-//             let spentValue = data.expenseSum;
-//             document.getElementById("spent").innerHTML = spentValue
-//         })
-//         .catch(error => (console.error('Error:', error)));
-// };
-
-// const getLimitForCategory = (id) => {
-//     fetch(`/api/limit/${id}`)
-//         .then(response => response.json())
-//         .then(data => {
-//             let limitValue = data.limit_value;
-//             document.getElementById("limit").innerHTML = limitValue
-//         })
-//         .catch(error => (console.error('Error:', error)));
-// };
-
-
-
-
-//Zmiana daty
-
-//Zmiana kategorii
-document.getElementById('expense_value').addEventListener('change', renderOnDom);
-document.getElementById('expense_date').addEventListener('change', renderOnDom);
-
-
+document.getElementById('expense_cat').addEventListener('change', isLimit);
+document.getElementById('expense_date').addEventListener('change', changeValues);
+document.getElementById('expense_value').addEventListener('change', changeValues);
+document.getElementById('expense_value').addEventListener('change', changeValues);
